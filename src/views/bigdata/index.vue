2c28afa6a3b9807c7a4edcf2de0af4263ddd5fcd
Caoyuan <964332498@QQ.COM>
2019-12-23 17:11:33 +0800

    调整大数据

--- src/views/bigdata/index.vue
+++ src/views/bigdata/index.vue
@@ -2,45 +2,69 @@
   <div class="dashboard-container">
     <div class="dashboard-text">
       <el-row :gutter="10">
+        <!--左边-->
         <el-col :span="3"><div class="content">
-          <div class="content-first border-r bg-color">
+          <div class="content-left-first border-r bg-color">
             <div class="content-text">
               <div class="content-text-label">地府总人数</div>
               <div class="content-text-number">192,168,552</div>
             </div>
           </div>
-          <div class="content-second border-r bg-color">
+          <div class="content-left-second border-r bg-color">
             <div class="content-text">
 
             </div>
           </div>
-          <div class="content-third border-r bg-color">
+          <div class="content-left-third border-r bg-color">
             <div class="content-text">
 
             </div>
           </div>
         </div></el-col>
-
+        <!--中间-->
         <el-col :span="15"><div class="content">
           <div ref="chart" class="mapCls"></div>
         </div></el-col>
-
+        <!--右边-->
         <el-col :span="6"><div class="content">
-          <div class="content-first bg-color">
-            <div class="content-text">
-              <div class="content-text-label">地府总人数</div>
-              <div class="content-text-number">192,168,552</div>
-            </div>
+          <div class="content-right-item">
+            <el-row :gutter="20">
+              <!--左边title-->
+              <el-col :span="3"><div class="content-right-item-left bg-color">
+                <div class="horizontal-line"></div>
+                <span>地府工作人员业绩统计</span>
+              </div></el-col>
+              <!--右边content-->
+              <el-col :span="21"><div class="content-right-item-right bg-color">
+                <div class="content-right-item-right-label">统计</div>
+                <div class="content-right-item-right-label">6542+13%</div>
+                <div class="content-right-item-right-label">总计 今年</div>
+                <div class="content-right-item-right-label">126  -6%</div>
+                <div class="content-right-item-right-label">分类 去年</div>
+                <div class="content-right-item-right-label">667 +23%</div>
+              </div></el-col>
+            </el-row>
           </div>
-          <div class="content-second bg-color">
-            <div class="content-text">
 
-            </div>
+          <div class="content-right-item">
+            <el-row :gutter="20">
+              <el-col :span="3"><div class="content-right-item-left bg-color">
+                <div class="horizontal-line"></div>
+                <span>冥币汇率变化</span>
+              </div></el-col>
+              <el-col :span="21"><div class="content bg-color">
+                <div id="mincoinRate " ref="mincoinRate" style="height: 100%"></div>
+              </div></el-col>
+            </el-row>
           </div>
-          <div class="content-third bg-color">
-            <div class="content-text">
-
-            </div>
+          <div class="content-right-item">
+            <el-row :gutter="20">
+              <el-col :span="3"><div class="content-right-item-left bg-color">
+                <div class="horizontal-line"></div>
+                <span>地府运营指标统计</span>
+              </div></el-col>
+              <el-col :span="21"><div class="content bg-color"></div></el-col>
+            </el-row>
           </div>
         </div></el-col>
       </el-row>
@@ -51,17 +75,134 @@
 
 <script>
   import {MapDrillDown} from '@/lib/map/echartMapOptions'
+  import echarts from '@/lib/echarts'
 
   export default {
     name: 'bigdata',
     data(){
       return{
         countManNumber:'192,168,1199',
-        lableImage:require('../../assets/images/lable_img.png')
+        lableImage:require('../../assets/images/lable_img.png'),
+        mincoinRate:'',
+        colors:['#5793f3', '#d14a61', '#675bba']
       }
     },
     mounted () {
       new MapDrillDown(this.$refs.chart, this).init()
+      this.mincoinRateDaw();
+      let that = this;
+      window.onresize = function () {
+        that.mincoinRate.resize();
+      }
+    },
+    methods:{
+      mincoinRateDaw(){
+        // 基于准备好的dom，初始化echarts实例
+        this.mincoinRate = echarts.init(this.$refs.mincoinRate)
+        // 绘制图表
+        this.mincoinRate.setOption(
+          {
+            color: this.colors,
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                type: 'cross'
+              }
+            },
+            grid: {
+              right: '40%',
+              left: '11%'
+            },
+            legend: {
+              data:['RMB','冥币','汇率']
+            },
+            xAxis: [
+              {
+                type: 'category',
+                axisTick: {
+                  alignWithLabel: true,
+                  lineStyle: {
+                    color: this.colors[0]
+                  }
+                },
+                axisLine: {
+                  lineStyle: {
+                    color: this.colors[2]
+                  }
+                },
+                data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+              }
+            ],
+            yAxis: [
+              {
+                type: 'value',
+                name: 'RMB',
+                min: 0,
+                max: 250,
+                position: 'right',
+                axisLine: {
+                  lineStyle: {
+                    color: this.colors[0]
+                  }
+                },
+                axisLabel: {
+                  formatter: '{value} ml'
+                }
+              },
+              {
+                type: 'value',
+                name: '冥币',
+                min: 0,
+                max: 250,
+                position: 'right',
+                offset: 80,
+                axisLine: {
+                  lineStyle: {
+                    color: this.colors[1]
+                  }
+                },
+                axisLabel: {
+                  formatter: '{value} ml'
+                }
+              },
+              {
+                type: 'value',
+                name: '汇率',
+                min: 0,
+                max: 25,
+                position: 'left',
+                axisLine: {
+                  lineStyle: {
+                    color: this.colors[2]
+                  }
+                },
+                axisLabel: {
+                  formatter: '{value} °C'
+                }
+              }
+            ],
+            series: [
+              {
+                name:'RMB',
+                type:'bar',
+                data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+              },
+              {
+                name:'冥币',
+                type:'bar',
+                yAxisIndex: 1,
+                data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+              },
+              {
+                name:'汇率',
+                type:'line',
+                yAxisIndex: 2,
+                data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+              }
+            ]
+          }
+        )
+      }
     }
   }
 </script>
@@ -84,17 +225,14 @@
       }
       .el-row{
         height: 100%;
-        margin-bottom: 20px;
-        &:last-child {
-           margin-bottom: 0;
-        }
         .el-col{
           height: 100%;
           border-radius: 4px;
           .content{
             height: 100%;
-            text-align: center;
-            padding: 10px;
+            .mapCls{
+              height: 100%;
+            }
             .content-text{
               margin: auto;
               left: 0;
@@ -104,9 +242,10 @@
               position: absolute;
               padding: 3px;
             }
-            .content-first{
+            .content-left-first{
               position: relative;
               min-height: 100px;
+              text-align: center;
               .content-text{
                 height: 52px;
                 color: #00FFCC;
@@ -123,18 +262,43 @@
                 }
               }
             }
-            .content-second{
+            .content-left-second{
               position: relative;
               height: 50%;
               min-height: calc(100vh - 300px);
               margin: 10px 0;
             }
-            .content-third{
+            .content-left-third{
               position: relative;
               min-height: 200px;
             }
-            .mapCls{
-              height: 100%;
+            .content-right-item{
+              position: relative;
+              color: #ffffff;
+              height: 33%;
+              &:nth-child(2){
+                margin: 10px 0;
+              }
+              .content-right-item-left{
+                height: 100%;
+                padding: 25px 7px;
+                text-align: center;
+                position: relative;
+                .horizontal-line{
+                  width: 100%;
+                  height: 5px;
+                  background: #00ffcc;
+                  display: inline-block;
+                  vertical-align: top;
+                }
+              }
+              .content-right-item-right{
+                height: 100%;
+                padding: 25px 7px;
+                .content-right-item-right-label{
+                  height: 16.666667%;
+                }
+              }
             }
           }
         }

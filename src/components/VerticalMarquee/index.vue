cc2a6dd63b14dd8383343ca38fe9cadd20d249d4
Caoyuan <964332498@QQ.COM>
2020-01-02 17:38:14 +0800

    大数据跑马灯

--- src/components/VerticalMarquee/index.vue
+++ src/components/VerticalMarquee/index.vue
@@ -2,36 +2,38 @@
   <div class="marquee">
     <div class="marquee-content">
       <div class="marquee-content-pre">
-        <a href="javascript:void(0)">
+        <a href="javascript:void(0)" @click="changeHell(-1)">
           <el-image style="width: 29px;height: 8px" :src="previousImage" fit="cover"></el-image>
         </a>
       </div>
       <div class="marquee-content-center">
-        <div class="bgImage invertedTrapezoid">铁磨地狱</div>
-        <div class="bgImage rectangle">孤独地狱</div>
+        <div class="bgImage invertedTrapezoid">{{getCurrentHellNameByIndex(-1)}}</div>
+        <div class="bgImage rectangle">{{currentHell.hellName}}</div>
         <div class="bgImage box">
           <div class="box-content">
             <div class="box-item">
               <div class="box-item-label">本层人数</div>
-              <el-progress :stroke-width="2" :percentage="20" :show-text="false"  :color="customColorMethod"></el-progress>
+              <el-progress :stroke-width="2" :percentage="currentHell.peopleNumber" :show-text="false"  :color="customColorMethod"></el-progress>
             </div>
             <div class="box-item">
               <div class="box-item-label">痛苦值</div>
-              <el-progress :stroke-width="2" :percentage="50" :show-text="false"  :color="customColorMethod"></el-progress>
+              <el-progress :stroke-width="2" :percentage="currentHell.painValue" :show-text="false"  :color="customColorMethod"></el-progress>
             </div>
             <div class="box-item">
               <div class="box-item-label">物资补给</div>
-              <el-progress :stroke-width="2" :percentage="80" :show-text="false"  :color="customColorMethod"></el-progress>
+              <el-progress :stroke-width="2" :percentage="currentHell.supplies" :show-text="false"  :color="customColorMethod"></el-progress>
             </div>
           </div>
         </div>
-        <div class="bgImage trapezoid">畜生地狱</div>
-        <div class="bgImage trapezoid">剥皮地狱</div>
-        <div class="bgImage trapezoid">沸屎地狱</div>
+        <div class="bgImage trapezoid">{{getCurrentHellNameByIndex(1)}}</div>
+        <div class="bgImage trapezoid">{{getCurrentHellNameByIndex(2)}}</div>
+        <div class="bgImage trapezoid">{{getCurrentHellNameByIndex(3)}}</div>
       </div>
-      <div class="marquee-content-next"><a href="javascript:void(0)">
-        <el-image style="width: 29px;height: 8px" :src="nextImage" fit="cover"></el-image>
-      </a></div>
+      <div class="marquee-content-next">
+        <a href="javascript:void(0)"  @click="changeHell(1)">
+          <el-image style="width: 29px;height: 8px" :src="nextImage" fit="cover"></el-image>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -46,8 +48,13 @@
       return{
         previousImage: previousImage,
         nextImage: nextImage,
-        hellArr:[{'hellName':'铁磨地狱','peopleNumber':'200','painValue':'10','supplies':'20'}]
+        currentHell: {},
+        currentIndex: 0,
+        hellArr:[]
       }
+    },
+    created(){
+      this.findAllHell();
     },
     methods:{
       customColorMethod(percentage) {
@@ -58,6 +65,50 @@
         }else {
           return '#3be6bc';
         }
+      },
+
+      findAllHell(){
+        this.hellArr=[{id:1,hellName:'拔舌地狱',peopleNumber:60,painValue:23,supplies:20},
+          {id:2,hellName:'剪刀地狱',peopleNumber:70,painValue:44,supplies:20},
+          {id:3,hellName:'铁树地狱',peopleNumber:80,painValue:55,supplies:20},
+          {id:4,hellName:'孽镜地狱',peopleNumber:10,painValue:66,supplies:20},
+          {id:5,hellName:'蒸笼地狱',peopleNumber:20,painValue:77,supplies:20},
+          {id:6,hellName:'铜柱地狱',peopleNumber:30,painValue:88,supplies:20},
+          {id:7,hellName:'刀山地狱',peopleNumber:50,painValue:99,supplies:20},
+          {id:8,hellName:'冰山地狱',peopleNumber:60,painValue:21,supplies:20},
+          {id:9,hellName:'油锅地狱',peopleNumber:90,painValue:12,supplies:20},
+          {id:10,hellName:'牛坑地狱',peopleNumber:1,painValue:34,supplies:20},
+          {id:11,hellName:'石压地狱',peopleNumber:50,painValue:70,supplies:20},
+          {id:12,hellName:'舂臼地狱',peopleNumber:80,painValue:90,supplies:20},
+          {id:13,hellName:'血池地狱',peopleNumber:90,painValue:40,supplies:20},
+          {id:14,hellName:'枉死地狱',peopleNumber:100,painValue:90,supplies:20},
+          {id:15,hellName:'磔刑地狱',peopleNumber:70,painValue:40,supplies:20},
+          {id:16,hellName:'火山地狱',peopleNumber:90,painValue:40,supplies:20},
+          {id:17,hellName:'石磨地狱',peopleNumber:60,painValue:80,supplies:20},
+          {id:18,hellName:'刀锯地狱',peopleNumber:40,painValue:90,supplies:20}]
+        this.currentIndex = 0
+        this.currentHell = this.hellArr[this.currentIndex]
+      },
+      changeHell(num){
+        this.currentIndex += num
+        if (num > 0 && this.currentIndex >= this.hellArr.length){
+          this.currentIndex = 0
+        }
+        if (num < 0 && this.currentIndex < 0){
+          this.currentIndex = this.hellArr.length-1
+        }
+        this.currentHell = this.hellArr[this.currentIndex];
+      },
+      getCurrentHellNameByIndex(index){
+        let requestIndex = this.currentIndex + index
+        console.log(requestIndex)
+        if (index > 0 && requestIndex >= this.hellArr.length){
+          requestIndex = requestIndex - this.hellArr.length;
+        }
+        if (index < 0 && requestIndex < 0){
+          requestIndex = requestIndex + this.hellArr.length;
+        }
+        return this.hellArr[requestIndex].hellName
       },
     }
   }
